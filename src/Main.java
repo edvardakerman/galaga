f1f61768364f684b8fbb6c711cd074267b19962a
edvardakerman <ed@sagovagen.se>
2023-05-31 17:52:30 +0200

    refactored

--- src/Main.java
+++ src/Main.java
@@ -1,113 +1,48 @@
-
-
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.util.Duration;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private ImageView imageView;
-    private ImageView laserBeamView;
-    private double imageViewX;
-    private double imageViewY;
-    private final double imageViewWidth = 40;
-    private final double imageViewHeight = 40;
-    private boolean shooting = false;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Create the image view object
-        Image image = new Image("https://art.pixilart.com/02d5fee1a00ae6b.png");
-        imageView = new ImageView(image);
-        imageViewX = 200;
-        imageViewY = 200;
-        imageView.setX(imageViewX);
-        imageView.setY(imageViewY);
-        imageView.setFitWidth(imageViewWidth);
-        imageView.setFitHeight(imageViewHeight);
-        
-        // Create the laser beam view object
-        Image laserBeamImage = new Image("https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/8c8b1d81151796e.png");
-        laserBeamView = new ImageView(laserBeamImage);
-        laserBeamView.setX(-100);
-        laserBeamView.setY(-100);
-        laserBeamView.setFitWidth(10);
-        laserBeamView.setFitHeight(40);
-
-        // Create the pane and add the image view to it
         Pane root = new Pane();
-        root.getChildren().addAll(imageView, laserBeamView);
-
-        // Create the scene
         Scene scene = new Scene(root, 400, 400);
         scene.setFill(javafx.scene.paint.Color.BLACK);
 
-        // Add key event handlers
+        Player player = new Player(scene.getWidth() / 2, scene.getHeight() / 2);
+
+        root.getChildren().addAll(player.getImageView(), player.getLaserBeamView());
+
         scene.setOnKeyPressed(e -> {
             switch (e.getCode()) {
                 case UP:
-                    imageViewY -= 10;
-                    imageView.setY(imageViewY);
+                    player.moveUp();
                     break;
                 case DOWN:
-                    imageViewY += 10;
-                    imageView.setY(imageViewY);
+                    player.moveDown();
                     break;
                 case LEFT:
-                    imageViewX -= 10;
-                    imageView.setX(imageViewX);
+                    player.moveLeft();
                     break;
                 case RIGHT:
-                    imageViewX += 10;
-                    imageView.setX(imageViewX);
+                    player.moveRight();
                     break;
                 case SPACE:
-                    if (!shooting) {
-                        shooting = true;
-                        double laserBeamX = imageViewX + imageViewWidth / 2 - laserBeamView.getFitWidth() / 2;
-                        double laserBeamY = imageViewY - laserBeamView.getFitHeight();
-                        laserBeamView.setX(laserBeamX);
-                        laserBeamView.setY(laserBeamY);
-                        startLaserBeamAnimation();
-                    }
+                    player.shoot();
                     break;
                 default:
                     break;
             }
         });
 
-        // Show the stage
-		primaryStage.setTitle("Galactica!");
+        primaryStage.setTitle("Galactica!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
-    
-    private void startLaserBeamAnimation() {
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), event -> {
-            double laserBeamY = laserBeamView.getY();
-            if (laserBeamY > 0) {
-                laserBeamY -= 2;
-                laserBeamView.setY(laserBeamY);
-            } else {
-                shooting = false;
-                laserBeamView.setX(-100);
-                laserBeamView.setY(-100);
-            }
-        }));
-        timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.play();
-    }
-    
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
---
+++ src/Player.java
@@ -0,0 +1,96 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+public class Player {
+
+    private ImageView imageView;
+    private ImageView laserBeamView;
+    private double x;
+    private double y;
+    private final double width = 40;
+    private final double height = 40;
+    private boolean shooting = false;
+
+    public Player(double x, double y) {
+        this.x = x;
+        this.y = y;
+        createImageView();
+        createLaserBeamView();
+    }
+
+    private void createImageView() {
+        Image image = new Image("https://art.pixilart.com/02d5fee1a00ae6b.png");
+        imageView = new ImageView(image);
+        imageView.setX(x);
+        imageView.setY(y);
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(height);
+    }
+
+    private void createLaserBeamView() {
+        Image laserBeamImage = new Image("https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/8c8b1d81151796e.png");
+        laserBeamView = new ImageView(laserBeamImage);
+        laserBeamView.setX(-100);
+        laserBeamView.setY(-100);
+        laserBeamView.setFitWidth(10);
+        laserBeamView.setFitHeight(40);
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public ImageView getLaserBeamView() {
+        return laserBeamView;
+    }
+
+    public void moveUp() {
+        y -= 10;
+        imageView.setY(y);
+    }
+
+    public void moveDown() {
+        y += 10;
+        imageView.setY(y);
+    }
+
+    public void moveLeft() {
+        x -= 10;
+        imageView.setX(x);
+    }
+
+    public void moveRight() {
+        x += 10;
+        imageView.setX(x);
+    }
+
+    public void shoot() {
+        if (!shooting) {
+            shooting = true;
+            double laserBeamX = x + width / 2 - laserBeamView.getFitWidth() / 2;
+            double laserBeamY = y - laserBeamView.getFitHeight();
+            laserBeamView.setX(laserBeamX);
+            laserBeamView.setY(laserBeamY);
+            startLaserBeamAnimation();
+        }
+    }
+
+    private void startLaserBeamAnimation() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), event -> {
+            double laserBeamY = laserBeamView.getY();
+            if (laserBeamY > 0) {
+                laserBeamY -= 2;
+                laserBeamView.setY(laserBeamY);
+            } else {
+                shooting = false;
+                laserBeamView.setX(-100);
+                laserBeamView.setY(-100);
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+}
