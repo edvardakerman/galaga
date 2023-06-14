d3e25aa811c6751b88c9f8fbf7200590300bfd0e
edvardakerman <ed@sagovagen.se>
2023-06-15 01:11:21 +0200

    added menu exit and welcome text

--- src/Menu.java
+++ src/Menu.java
@@ -1,14 +1,22 @@
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class Menu extends VBox {
 
-    private Button startButton;
+    private Button startButton, exitButton;
+    private Text welcomeText;
     private Runnable onStartGame;
 
     public Menu() {
         setStyle("-fx-background-color: #222222;");
         setSpacing(20);
+        
+        welcomeText = new Text(180, 50, "Welcome to Galactica!!");
+        welcomeText.setFont(Font.font("Arial", 18));
+        welcomeText.setFill(Color.WHITE);
 
         startButton = new Button("Start Game");
         startButton.setOnAction(event -> {
@@ -16,8 +24,13 @@ public class Menu extends VBox {
                 onStartGame.run();
             }
         });
+        
+        exitButton = new Button("Exit Game");
+        exitButton.setOnAction(event -> {
+        	System.exit(0);
+        });
 
-        getChildren().addAll(startButton);
+        getChildren().addAll(welcomeText, startButton, exitButton);
     }
 
     public void setOnStartGame(Runnable onStartGame) {
